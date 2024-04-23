ef7f786bc53392d59c0cd7179e84b3d701feee25
Ev Shevchenko <2058931@gmail.com>
2024-04-23 16:19:46 +0300

    Base v0.3

--- main.py
+++ main.py
@@ -18,9 +18,13 @@ import DirModel
 import FileModel
 import EmptyFileModel
 
+from misc import FileWorker
+
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
+
+    fw = FileWorker('x')
 
     dirModel = DirModel.DirModel(path = Path(__file__).resolve().parent)
     engine.rootContext().setContextProperty("modelDir", dirModel)
